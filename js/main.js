dd0085907ba9abd63a33ab2034d9c4c28aae4e47
Shannon Hawn <shannonhawn@hotmail.com>
2016-04-07 18:17:59 -0400

    added variables and functions

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var $banner = $('.banner');
+var $addStars;
+var $width = $banner.outerWidth();
+var $height = $banner.outerHeight();
+
+for (i = 0; i < 75; i++) {
+  $rotateNum = Math.random() * 1000;
+  $scaleNum = Math.random() * 2;
+  $animateNum = Math.random() * 10;
+  $addStars = $('<div>');
+}
