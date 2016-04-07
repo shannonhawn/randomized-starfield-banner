851181285bc802cf7f271fd4e13f7c819da3d0cd
Shannon Hawn <shannonhawn@hotmail.com>
2016-04-07 18:22:53 -0400

    amended spacing for Markbot

--- js/main.js
+++ js/main.js
@@ -8,4 +8,15 @@ for (i = 0; i < 75; i++) {
   $scaleNum = Math.random() * 2;
   $animateNum = Math.random() * 10;
   $addStars = $('<div>');
-}
+  $addStars.addClass('star');
+  $addStars.css({
+    left: Math.random() * $width,
+    top: Math.random() * $height,
+    opacity: Math.random() * 10,
+    width: Math.random() * 100,
+    transform: 'rotate(' + $rotateNum + 'deg) scale(' + $scaleNum + ')',
+    animation: 'growstars' + $animateNum + 's alternate infinite'
+  });
+
+  $banner.append($addStars);
+};
